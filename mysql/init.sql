fdb5b522ee76814641bf0d9dc38067e2bed7db06
Timotej <timi.blazic@gmail.com>
2026-01-26 12:25:00 +0100

    added order-service to local dev

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE IF NOT EXISTS cloudshopt_users;
 CREATE DATABASE IF NOT EXISTS cloudshopt_products;
+CREATE DATABASE IF NOT EXISTS cloudshopt_orders;
 
 GRANT ALL PRIVILEGES ON cloudshopt_users.* TO 'cloudshoptuser'@'%';
 GRANT ALL PRIVILEGES ON cloudshopt_products.* TO 'cloudshoptuser'@'%';
+GRANT ALL PRIVILEGES ON cloudshopt_orders.* TO 'cloudshoptuser'@'%';
 FLUSH PRIVILEGES;
