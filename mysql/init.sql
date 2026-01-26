97fef530e02ef59f5658e680e0ea2d14078d60a6
Timotej <timi.blazic@gmail.com>
2026-01-26 12:35:04 +0100

    added payment-service to local dev

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,8 +1,10 @@
 CREATE DATABASE IF NOT EXISTS cloudshopt_users;
 CREATE DATABASE IF NOT EXISTS cloudshopt_products;
 CREATE DATABASE IF NOT EXISTS cloudshopt_orders;
+CREATE DATABASE IF NOT EXISTS cloudshopt_payments;
 
 GRANT ALL PRIVILEGES ON cloudshopt_users.* TO 'cloudshoptuser'@'%';
 GRANT ALL PRIVILEGES ON cloudshopt_products.* TO 'cloudshoptuser'@'%';
 GRANT ALL PRIVILEGES ON cloudshopt_orders.* TO 'cloudshoptuser'@'%';
+GRANT ALL PRIVILEGES ON cloudshopt_payments.* TO 'cloudshoptuser'@'%';
 FLUSH PRIVILEGES;
